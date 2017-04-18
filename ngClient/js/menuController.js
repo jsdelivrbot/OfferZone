49e4b9bbe04fa1cb6d1cf60c34793de956f045c4
Sanjay <yadavsanjay@hotmail.com>
2017-04-18 18:49:42 +0530

    Update menuController.js

--- ngClient/js/menuController.js
+++ ngClient/js/menuController.js
@@ -1,46 +1,71 @@
-myApp.controller("MenuCtrl", ['$scope', '$location','MenuFactory',
-  function($scope, $location,MenuFactory) {
-      $scope.success=false;
-
-     $scope.menus='';
-
-      MenuFactory.getAllmenu().success(function(data){
-        console.log('ddd'+data);
-        $scope.menus=data;
-      });
+myApp.controller("MenuCtrl", ['$scope', '$location', 'MenuFactory',
+    function($scope, $location, MenuFactory) {
+        $scope.success = false;
+        $scope.dateRange = null;
+        $scope.offer = {};
 
 
-    $scope.isActive = function(route) {
-      return route === $location.path();
-    }
+        $scope.menus = '';
 
-    $scope.genPDF=function(){
-      console.log('pdfgen');
-
-       html2canvas(document.getElementById('print'), {
-            onrendered: function (canvas) {
-         
-                var data = canvas.toDataURL();
-                var docDefinition = {
-                    content: [{
-                        image: data,
-                        width: 500,
-                    }]
-                };
-                pdfMake.createPdf(docDefinition).download("Score_Details.pdf");
-            }
+        MenuFactory.getAllmenu().success(function(data) {
+            console.log('ddd' + data);
+            $scope.menus = data;
         });
 
+
+        $scope.isActive = function(route) {
+            return route === $location.path();
+        }
+        $scope.happyHrs = function($isHappyHr) {
+            if ($isHappyHr == true) {
+                $scope.showHappy = true;
+            } else {
+                $scope.showHappy = false;
+            }
+
+        }
+
+        $scope.onCategoryChange = function($selval) {
+            if ($selval == 4) {
+                $scope.showDays = true;
+            } else {
+                $scope.showDays = false;
+            }
+        }
+
+        $scope.genPDF = function() {
+            console.log('pdfgen');
+
+            html2canvas(document.getElementById('print'), {
+                onrendered: function(canvas) {
+
+                    var data = canvas.toDataURL();
+                    var docDefinition = {
+                        content: [{
+                            image: data,
+                            width: 500,
+                        }]
+                    };
+                    pdfMake.createPdf(docDefinition).download("Score_Details.pdf");
+                }
+            });
+
+        }
+
+        $scope.createMenu = function() {
+
+            /**
+             MenuFactory.create($scope.menu).success(function(data){
+              $scope.menus.push($scope.menu);
+               $scope.success=true;
+               $scope.menu='';
+
+              });
+             */
+
+            console.log($scope.offer);
+
+
+        }
     }
-
-    $scope.createMenu=function(){
-
-    MenuFactory.create($scope.menu).success(function(data){
-    $scope.menus.push($scope.menu);
-     $scope.success=true;
-     $scope.menu='';
-
-    });
-    }
-  }
 ]);
