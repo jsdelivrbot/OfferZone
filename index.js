b7efbe77c0ef2b53ccd1907d388307c8d3527971
Sanjay <yadavsanjay@hotmail.com>
2017-05-15 15:00:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,12 +6,6 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/ngClient'));
 
 
-app.get('/', function(request, response) {
-  response.json({
-        "status": 200,
-        "message": "Invalid credentials"
-      });
-});
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
